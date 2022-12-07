f18ad726fa32f445030e39f05d643baf8e40f64f
gileduardo <gilgea@gmail.com>
2022-12-07 11:15:18 -0300

    Aula Revisão - PACMAN

---
+++ 16 - Revisão PACMAN/gea.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define COR_PRETO 		printf("\33[30m");
+#define COR_VERMELHO 	printf("\33[31m");
+#define COR_VERDE 		printf("\33[32m");
+#define COR_LARANJA 	printf("\33[33m");
+#define COR_AZUL 		printf("\33[34m");
+#define COR_ROXO		printf("\33[35m");
+#define COR_AGUA		printf("\33[36m");
+#define COR_BRANCO 		printf("\33[37m");
+
+
---
+++ 16 - Revisão PACMAN/pacman.c
@@ -0,0 +1,10 @@
+#include "keyboard.h"
+#include "gea.h"
+
+int main() {
+		
+	printf("\n");
+	return 0;
+}
+
+
